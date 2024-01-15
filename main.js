1bac78620a32661391c8eaf335b1b88f7dcd3ee2
GloQue <gaaquaye@gmail.com>
2024-01-15 11:32:31 +0000

    first output commit

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ const addCount = () => {
 }
 
 console.log(addCount())
+
+
+const arr = [1, 2, 3, 4]
+
+const double = (item) => {
+    return item * 2
+}
+
+let valOne = arr.map(double)
+
+console.log(valOne)
